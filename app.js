e2289a50fa68affb50a5c7c978a2eac71dcaeb50
atrillionthpiece <89700888+atrillionthpiece@users.noreply.github.com>
2026-03-03 14:50:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let resetBtn=document.querySelector(".reset-btn");
 let newGameBtn= document.querySelector(".new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg=document.querySelector(".msg");
+let winSound = new Audio("victory.mp3");
+let drawSound = new Audio("draw.mp3");
+let buttonSound = new Audio("button1.mp3");
 let turnO= true;
 const winPatters=[
     [0,1,2],
@@ -23,6 +26,8 @@ const resetGame = () =>{
 
 boxes.forEach((box) => {
     box.addEventListener("click",()=>{
+        buttonSound.currentTime=0;
+        buttonSound.play();
         console.log("box was clicked");
         if(turnO){
             box.innerText="O";
@@ -57,7 +62,9 @@ const showWinner= (winner) =>{
 };
 
 const checkWinner = () =>{
+    let isDraw=true;
     for(pattern of winPatters){
+        
         let pos1Val =boxes[pattern[0]].innerText;
         let pos2Val =boxes[pattern[1]].innerText;
         let pos3Val =boxes[pattern[2]].innerText;
@@ -65,10 +72,31 @@ const checkWinner = () =>{
             if (pos1Val === pos2Val && pos2Val === pos3Val){
                 console.log("winner", pos1Val);
                 showWinner(pos1Val);
+                winSound.currentTime=0;
+                winSound.play();
+                return;
             }
         }
     };
+    boxes.forEach((box)=>{
+        if(box.innerText ===""){
+            isDraw=false;
+        }
+    })
+    if (isDraw){
+        gameDraw();
+        drawSound.currentTime=0;
+        drawSound.play();
+    }
 };
 
-newGameBtn.addEventListener("click", resetGame);
-resetBtn.addEventListener("click",resetGame);
+newGameBtn.addEventListener("click", () =>{
+    buttonSound.currentTime=0;
+    buttonSound.play();
+    resetGame();
+})
+resetBtn.addEventListener("click", ()=>{
+    buttonSound.currentTime=0;
+    buttonSound.play();
+    resetGame();
+});
